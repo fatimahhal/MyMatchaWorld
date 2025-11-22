5dc0d396cc83279308557a04287b7c52fc874bef
Fatimah Albariqi <fatemahh744@gmail.com>
2025-11-22 16:07:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const images = document.querySelectorAll('img');
-images.forEach(img => {
-    img.addEventListener('click', function() {
-    img.style.width = "100%";
-    img.style.height = "auto";
-    });
-});
+function upDate(previewPic){
+    document.getElementById('image').style.backgroundImage = `url(${previewPic.src})`;
+    document.getElementById('image').textContent = previewPic.alt;
+}
+
+function unDo(){
+    document.getElementById('image').style.backgroundImage = "url('')";
+    document.getElementById('image').textContent = "Hover over an image below to display here.";
+}
